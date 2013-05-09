e676b4734436d822b5177e8699e9a4b034a50e1a
Remo Mueller <remosm@gmail.com>
2013-05-09 15:50:08 -0400

    Tests now check for valid variable types and valid json files

--- test/validation/dictionary_test.rb
+++ test/validation/dictionary_test.rb
@@ -3,12 +3,45 @@ require 'test_helper'
 
 class DictionaryTest < Test::Unit::TestCase
 
-  def test_valid_json
-    assert_equal true, (JSON.parse("filename...") rescue false)
+  def test_check_for_invalid_variable_types
+    dictionary_files.each do |file|
+      assert_equal "#{file} has valid variable type", file_valid_variable_type(file)
+    end
   end
 
-  def test_check_for_invalid_variable_types
-    assert_equal true, ['identifier', 'choices', 'integer', 'numeric'].include?("GET FROM JSON.")
+  def test_valid_json
+    dictionary_files.each do |file|
+      assert_equal "#{file} is valid JSON", file_valid_string(file)
+    end
+  end
+
+  def dictionary_files
+    Dir.glob("dictionary/**/*.json")
+  end
+
+  def file_valid_string(file)
+    result = "#{file}"
+    if (!!JSON.parse(File.read(file)) rescue false)
+      result += " is valid JSON"
+    else
+      result += " is not valid JSON"
+    end
+    result
+  end
+
+  def file_valid_variable_type(file)
+    valid_variable_types = ['identifier', 'choices', 'integer', 'numeric']
+    result = "#{file}"
+    if (json = JSON.parse(File.read(file)) rescue false)
+      if valid_variable_types.include?(json["type"])
+        result += " has valid variable type"
+      else
+        result += " variable type #{json["type"].inspect} is not a valid variable type: #{valid_variable_types}"
+      end
+    else
+      result += " is not valid JSON"
+    end
+    result
   end
 
 end
