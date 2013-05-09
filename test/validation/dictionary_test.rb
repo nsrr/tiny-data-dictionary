de680e9bdf5219a359fb5f864529051e0ed45a14
Remo Mueller <remosm@gmail.com>
2013-05-09 15:22:16 -0400

    Parse JSON in DD

--- test/validation/dictionary_test.rb
+++ test/validation/dictionary_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 class DictionaryTest < Test::Unit::TestCase
 
   def test_valid_json
-    assert_equal true, false # (JSON.parse("filename...") rescue false)
+    assert_equal true, (JSON.parse("filename...") rescue false)
   end
 
   def test_check_for_invalid_variable_types
